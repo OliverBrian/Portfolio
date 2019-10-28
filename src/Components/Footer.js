3089bf11bd14d95d301f21b119d728a5590ff7a2
Oliver Brian Barra Vasquez <oliver.vasquez@student.kyh.se>
2019-10-28 16:59:06 +0100

    added blank on footer icons

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -9,13 +9,13 @@ class Footer extends Component {
                     <a href="mailto:oliver.brian.vasquez@hotmail.se" className="mail">
                         <i className="fa fa-envelope-square"/>
                     </a>
-                    <a href="https://github.com/OliverBrian" className="github">
+                    <a href="https://github.com/OliverBrian" target="blank" className="github">
                         <i className="fab fa-github-square"/>
                     </a>
-                    <a href="https://www.facebook.com/oliver.b.vasquez" className="facebook">
+                    <a href="https://www.facebook.com/oliver.b.vasquez" target="blank" className="facebook">
                         <i className="fab fa-facebook-square"/>
                     </a>
-                    <a href="https://www.instagram.com/oliverbrian/" className="instagram">
+                    <a href="https://www.instagram.com/oliverbrian/" target="blank" className="instagram">
                         <i className="fab fa-instagram"/>
                     </a>
                 </div>
